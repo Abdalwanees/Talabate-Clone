9ca2a6c8c84cbb9a4c95acd0aa28ef246ca8d3b4
Abdalwanees <gabdo7682@gmail.com>
2024-09-30 22:22:32 +0300

    Create Repositores

---
+++ Talabate.Clone.Core/Repository.Contruct/IGenaricRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabate.Clone.Core.Entites;
+
+namespace Talabate.Clone.Core.Repository.Contruct
+{
+    public interface IGenaricRepository<T> where T : BaseEntity
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetAsync(int Id);
+    }
+}
---
+++ Talabate.Clone.Repository/Repositories/GenaricRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabate.Clone.Core.Entites;
+using Talabate.Clone.Core.Repository.Contruct;
+using Talabate.Clone.Repository.Data.Contexts;
+
+namespace Talabate.Clone.Repository.Repositories
+{
+    public class GenaricRepository<T> : IGenaricRepository<T> where T : BaseEntity
+    {
+        private readonly StoreDbContext _dbContext;
+
+        public GenaricRepository(StoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+           return await _dbContext.Set<T>().AsNoTracking().ToListAsync();
+        }
+
+        public async Task<T?> GetAsync(int Id)
+        {
+            //if (typeof(T)==typeof(Product))
+            //{
+            //    return  await _dbContext.Set<Product>().AsNoTracking().FirstOrDefaultAsync(p => p.Id == Id);
+            //}
+            return await _dbContext.Set<T>().FindAsync(Id);
+        }
+    }
+}
