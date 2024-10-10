a160892aba1d398bfc55fb3fe7096fb7a383bb88
gabdo7682@gmail.com <gabdo7682@gmail.com>
2024-10-11 00:21:46 +0300

    Consume controller Buggy controller to represent all type of error

---
+++ Talabate.Clone.API/Controllers/BuggyController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Talabate.Clone.Repository.Data.Contexts;
+
+namespace Talabate.Clone.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BuggyController : ControllerBase
+    {
+        private readonly StoreDbContext _context;
+
+        public BuggyController(StoreDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("NotFound")] 
+        public ActionResult GetNotFoundError()
+        {
+            var product = _context.Products.Find(100);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+        [HttpGet("ServrError")]
+        public ActionResult GetServerError()
+        {
+            var product=_context.Products.Find(100);
+            var productDto = product.ToString();
+            return Ok(productDto);
+        }
+        [HttpGet("BadRequest")]
+        public ActionResult GetBadRequest() {
+        return BadRequest();
+        }
+        [HttpGet("BadRequest/{Id}")]
+        public ActionResult GetBadRequest(int Id) {
+        return Ok();
+        }
+
+        // try to test end point not found
+    }
+}
