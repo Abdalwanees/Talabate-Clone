565ae42683f84dac06c708b4f030c2d6db9b8605
gabdo7682@gmail.com <gabdo7682@gmail.com>
2024-10-12 01:38:38 +0300

    Clean up program.cs and refactor my code    use class for middlware and class for services

---
+++ Talabate.Clone.API/Extentions/ApiApplicationServicesExtention.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Talabate.Clone.API.Errors;
+using Talabate.Clone.API.Helpers;
+using Talabate.Clone.Core.Repository.Contruct;
+using Talabate.Clone.Repository.Data.Contexts;
+using Talabate.Clone.Repository.Repositories;
+
+namespace Talabate.Clone.API.Extensions
+{
+    public static class ApiApplicationServicesExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Register the DbContext and configure the connection string from configuration
+            services.AddDbContext<StoreDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnction"));
+            });
+
+            // Register Generic Repository Services
+            services.AddScoped(typeof(IGenaricRepository<>), typeof(GenaricRepository<>));
+
+            // Register AutoMapper Services
+            services.AddAutoMapper(typeof(MappingProfile));
+
+            // Register validation error configuration for API responses
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = (actionContext) =>
+                {
+                    var errors = actionContext.ModelState
+                                              .Where(e => e.Value.Errors.Count > 0)
+                                              .SelectMany(e => e.Value.Errors)
+                                              .Select(e => e.ErrorMessage).ToList();
+
+                    var response = new ApiValidationResponse
+                    {
+                        Errors = errors
+                    };
+
+                    return new BadRequestObjectResult(response);
+                };
+            });
+            return services;
+        }
+    }
+}
