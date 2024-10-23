60c1f2c952fa3d3fcc6a6fd92a84e6e7eb0b4d6b
gabdo7682@gmail.com <gabdo7682@gmail.com>
2024-10-23 15:50:39 +0300

    Add Basket Module

---
+++ Talabate.Clone.API/DTOs/BasketItemDto.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Talabate.Clone.API.DTOs
+{
+    public class BasketItemDto
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string ProductName { get; set; }
+
+        [Required]
+        [Range(0.1, double.MaxValue, ErrorMessage = "Invalid Price, Must be greater than Zero")]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string PictureUrl { get; set; }
+
+        [Required]
+        public string Brand { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Invalid Quantity, Must be at least one or greater than Zero")]
+        public int Quantity { get; set; }
+    }
+}
---
+++ Talabate.Clone.Core/Repository.Contruct/IBasketRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabate.Clone.Core.Entites.Busket;
+
+namespace Talabate.Clone.Core.Repository.Contruct
+{
+    public interface IBasketRepository
+    {
+        Task<CustomerBasket?> GetBasketAsync(string basketId);
+        Task<CustomerBasket?> UpdatetAsync(CustomerBasket basket);
+        Task<bool> DeleteBasketAsync(string basketId);
+    }
+}
