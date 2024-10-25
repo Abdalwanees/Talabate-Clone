496ec4831ec497183e0c5e0aaa718d9d42313c9a
gabdo7682@gmail.com <gabdo7682@gmail.com>
2024-10-25 11:02:13 +0300

    Create buisness of OrderModule Service

---
+++ Talabate.Clone.Core/Services.Contruct/IOrderService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabate.Clone.Core.Entites.Order.Aggregrate;
+
+namespace Talabate.Clone.Core.Services.Contruct
+{
+    public interface IOrderService
+    {
+        Task<Order> CreateOrderAsync(string buyerEmail ,string basketId,int delivaryMethod ,Address address);
+        Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string buyerEmail);
+        Task<Order> GetOrderByIdForUserAsync(int orderId, string buyerEmail);
+    }
+}
---
+++ Talapate.Clone.Services/Services/OrderService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabate.Clone.Core.Entites.Order.Aggregrate;
+using Talabate.Clone.Core.Services.Contruct;
+
+namespace Talapate.Clone.Services.Services
+{
+    public class OrderService : IOrderService
+    {
+        public Task<Order> CreateOrderAsync(string buyerEmail, string basketId, int delivaryMethod, Address address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Order> GetOrderByIdForUserAsync(int orderId, string buyerEmail)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string buyerEmail)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
