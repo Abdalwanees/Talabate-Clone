c4fbf173dd629248abf014071d61182c4a988a35
gabdo7682@gmail.com <gabdo7682@gmail.com>
2024-10-29 18:22:52 +0300

    Refactor returning response to orderToReturnDto

---
+++ Talabate.Clone.API/DTOs/OrderItemDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Talabate.Clone.API.DTOs
+{
+    public class OrderItemDto
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string PictureUrl { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
